79384bb397b7d78deb1089e2bace1b648580d754
ilya <011283im@gmail.com>
2022-11-30 15:27:51 +0300

    создали первичную структуру для чистой архитектуры

--- app/src/main/java/com/example/androidtrnslator/app/MainActivity.kt
+++ app/src/main/java/com/example/androidtrnslator/app/MainActivity.kt
@@ -1,7 +1,8 @@
-package com.example.androidtrnslator
+package com.example.androidtrnslator.app
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.androidtrnslator.R
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
